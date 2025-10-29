c4c3e70f86c0749e0022d7d5aacf720b833dccb2
samir <samirphapale1997@gmail.com>
2025-10-29 15:33:22 +0530

    simple html

---
+++ js/jsscript.js
@@ -0,0 +1,4 @@
+document.getElementById("helloBtn").addEventListener("click", () => {
+  const msg = "🎉 Hello from your modern Nginx website!";
+  document.getElementById("output").textContent = msg;
+});
